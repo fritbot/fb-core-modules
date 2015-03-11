c0273959c3158ba22faa90538831f1b0d2372ac6
Lee Goolsbee (lee) <lee@edgecase.io>
2015-03-10 21:58:18 -0500

    Better comments

--- src/weather.js
+++ src/weather.js
@@ -19,18 +19,21 @@ function getWeather(route, args) {
 		var results = JSON.parse(body),
 			data = results.current_observation;
 
-		// If we have data, return a formatted version.
 		if (data) {
+			// If we have weather data, return a formatted version
 			route.send(data.display_location.full + ': ' + data.weather + '; ' + data.temp_f + '° F (feels like ' + data.feelslike_f + '° F); winds ' + data.wind_string.substring(0, 1).toLowerCase() + data.wind_string.substring(1));
 		} else if (results.response.results) {
+			// If Wunderground returns a list of cities, list them
 			var possibilities = [];
 			for (var index in results.response.results)
 			    possibilities.push('     ' + (parseInt(index) + 1) + '. ' + results.response.results[index].city + ', ' + results.response.results[index].state + ' ' + results.response.results[index].country);
 			route.send('Wunderground found a few possibilities; try one of the following instead:\n' + possibilities.join('\n'));
 		} else if (results.response.error) {
+			// If Wunderground returns an error, print it
 			route.send('Wunderground says: "' + results.response.error.description + '"');
 		} else {
-			route.send("I don't know what you wanted, but you're not getting the weather with that query.");
+			// If we get this far, reply with a witty message
+			route.send("I don't know what you wanted, but you're not getting it (unknown error).");
 		}
 
 	}.bind(this));
