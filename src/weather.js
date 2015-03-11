8c292617fd255eb31f0a40e1b7d1526f1433f06d
Lee Goolsbee (lee) <lee@edgecase.io>
2015-03-10 21:53:47 -0500

    Weather module enhancements

--- src/weather.js
+++ src/weather.js
@@ -4,13 +4,13 @@ var querystring = require('querystring'),
 function getWeather(route, args) {
 
 	// API Key for Wunderground.com – no idea who "owns" this key
-	apiKey = '83f199a422e382c3';
+	var apiKey = '83f199a422e382c3';
 
 	// Parse location from the args; fall back to Austin if no location was passed
-	location = args.length ? querystring.escape(args) : 'TX/Austin';
+	var location = args.length ? querystring.escape(args.map(function(x){return x.replace(/,/g, '');})) : 'TX/Austin';
 
 	// Set up the URL for the request
-	url = 'http://api.wunderground.com/api/' + apiKey + '/conditions/q/' + location + '.json';
+	var url = 'http://api.wunderground.com/api/' + apiKey + '/conditions/q/' + location + '.json';
 
 	// Make the request
 	request.get(url, function (err, res, body) {
@@ -21,9 +21,16 @@ function getWeather(route, args) {
 
 		// If we have data, return a formatted version.
 		if (data) {
-			route.send('Current Weather for ' + data.display_location.full + ': ' + data.weather + ', ' + data.temperature_string);
+			route.send(data.display_location.full + ': ' + data.weather + '; ' + data.temp_f + '° F (feels like ' + data.feelslike_f + '° F); winds ' + data.wind_string.substring(0, 1).toLowerCase() + data.wind_string.substring(1));
+		} else if (results.response.results) {
+			var possibilities = [];
+			for (var index in results.response.results)
+			    possibilities.push('     ' + (parseInt(index) + 1) + '. ' + results.response.results[index].city + ', ' + results.response.results[index].state + ' ' + results.response.results[index].country);
+			route.send('Wunderground found a few possibilities; try one of the following instead:\n' + possibilities.join('\n'));
+		} else if (results.response.error) {
+			route.send('Wunderground says: "' + results.response.error.description + '"');
 		} else {
-			route.send("Sorry, Wunderground doesn't seem to know anything about the weather for that location.");
+			route.send("I don't know what you wanted, but you're not getting the weather with that query.");
 		}
 
 	}.bind(this));
